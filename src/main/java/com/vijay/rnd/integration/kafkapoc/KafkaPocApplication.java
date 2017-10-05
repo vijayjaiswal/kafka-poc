ad8dd0e1796719fe1f1928fb647726a403372220
Vijay Jaiswal <vijay.jaiswal@birdeye.com>
2017-10-05 14:50:33 +0530

    first commit
    first commit

---
+++ src/main/java/com/vijay/rnd/integration/kafkapoc/KafkaPocApplication.java
@@ -0,0 +1,12 @@
+package com.vijay.rnd.integration.kafkapoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaPocApplication.class, args);
+	}
+}
